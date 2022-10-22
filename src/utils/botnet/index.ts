035df421527ed4c0aacb94938e0f42dc1e02eeba
Dave Freeman <sugardave@gmail.com>
2022-10-22 01:33:18 -0500

    add index for all botnet methods

---
+++ src/utils/botnet/index.ts
@@ -0,0 +1,35 @@
+import {addBot} from 'utils/botnet/addBot';
+import {addBotnet} from 'utils/botnet/addBotnet';
+import {botnetActions} from 'utils/botnet/botnetActions';
+import {botnetFlagsSchema} from 'utils/botnet/botnetFlagsSchema';
+import {botnetMap} from 'utils/botnet/botnetMap';
+import {cacheBotnetMap} from 'utils/botnet/cacheBotnetMap';
+import {deployScripts} from 'utils/botnet/deployScripts';
+import {generateBotnetName} from 'utils/botnet/generateBotnetName';
+import {getBotnetStatus} from 'utils/botnet/getBotnetStatus';
+import {getServerPriceList} from 'utils/botnet/getServerPriceList';
+import {hydrateBotnetMap} from 'utils/botnet/hydrateBotnetMap';
+import {ramOptions} from 'utils/botnet/ramOptions';
+import {removeBot} from 'utils/botnet/removeBot';
+import {removeBotnet} from 'utils/botnet/removeBotnet';
+import {startAttack} from 'utils/botnet/startAttack';
+import {stopAttack} from 'utils/botnet/stopAttack';
+
+export {
+    addBot,
+    addBotnet,
+    botnetActions,
+    botnetFlagsSchema,
+    botnetMap,
+    cacheBotnetMap,
+    deployScripts,
+    generateBotnetName,
+    getBotnetStatus,
+    getServerPriceList,
+    hydrateBotnetMap,
+    ramOptions,
+    removeBot,
+    removeBotnet,
+    startAttack,
+    stopAttack
+};
