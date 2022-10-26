b2483bc561e6dc3b314263cc80c4ff36bda76635
Dave Freeman <sugardave@gmail.com>
2022-10-25 22:56:50 -0500

    set explicit type for return of map hydration

--- src/utils/botnet/hydrateBotnetMap.ts
+++ src/utils/botnet/hydrateBotnetMap.ts
@@ -2,16 +2,18 @@ import {NS} from '@ns';
 import {fileLocations, MapFile} from 'utils/io/index';
 import {botnetMap} from 'utils/botnet/botnetMap';
 import {stashData} from 'utils/data/index';
+import {BotnetMap} from 'global';
 
 const hydrateBotnetMap = (ns: NS, mapType = 'all') => {
     const {location, suffix} = fileLocations.botnetMapCache;
     const mapFile = new MapFile(ns, `${mapType}${suffix}`, location);
     const contents = mapFile.read() || '[]';
     const parsed = JSON.parse(contents);
-    const result = new Map([...(parsed.length ? parsed : botnetMap)]);
+    const result: BotnetMap = new Map([
+        ...(parsed.length ? parsed : botnetMap)
+    ]);
 
     stashData({data: contents, stashName: 'botnetMap'});
-
     return result;
 };
 
